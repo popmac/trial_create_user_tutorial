ccc2cc938f791590e970d774cc69166bcc640214
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-02-28 22:54:39 +0900

    Sessionsコントローラーにログインのアクションを追加した。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      flash[:notice] = 'ログインしました。'
+      redirect_to user
+    else
+      flash.now[:danger] = 'Eメールもしくはパスワードが正しくありません。'
+      render 'new'
+    end
+  end
+
 end

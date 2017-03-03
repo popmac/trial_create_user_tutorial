2bf86f65762a55b46d081fd15140e29bc453a5f8
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-04 00:19:56 +0900

    アカウントの有効化リンクのメール送信とアカウントの有効化を切り出してメソッド化した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:notice] = "アカウントを有効化するためにメールを確認してください"
       redirect_to users_path
     else

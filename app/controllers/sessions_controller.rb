92a33dc71a3a9883e6411d9a4e8b9aa2efa36650
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-03 23:34:33 +0900

    アカウントが有効化されていたらログインできるようにした。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in(user)
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      flash[:notice] = 'ログインしました。'
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "アカウントが有効化されていません。"
+        message += "メールを確認してください。"
+        flash[:notice] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Eメールもしくはパスワードが正しくありません。'
       render 'new'

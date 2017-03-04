df3177372ec15fadd598a7d064d03c9a317de043
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-04 17:21:50 +0900

    password_resetsのeditアクションとビューを追加した。

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,6 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
 
   def new
   end
@@ -15,4 +17,22 @@ class PasswordResetsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    # 最初にget_userとvalid_userが実行される
+  end
+
+  private
+    # 以下はbeforeフィルター
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # 有効なユーザーかどうか確認する
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
 end

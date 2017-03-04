0f8c58f8f372193b3a6ae0c572ef4237aa89e774
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-04 11:14:58 +0900

    リファクタリングした。アカウントの有効化に関して実行される順番に並ぶようにした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,15 +40,15 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  # 有効化用のメールを送信する
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   # アカウントを有効にする
   def activate
     update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
-  end
-
-  # 有効化用のメールを送信する
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
   end
 
   private
